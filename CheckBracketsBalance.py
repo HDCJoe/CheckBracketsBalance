fcd33082c26c4865f7e2223dbe2aa9804088b34a
HDCJoe <joe.kennedy@hdc.govt.nz>
2026-06-25 16:47:16 +1200

    Initial Version

---
+++ CheckBracketsBalance.py
@@ -0,0 +1,28 @@
+def check_brackets(s):
+    pairs = {')': '(', ']': '[', '}': '{'}
+    stack = []
+
+    for i, ch in enumerate(s, start=1):
+        if ch in '([{':
+            stack.append((ch, i))
+        elif ch in ')]}':
+            if not stack:
+                return f"Extra closing '{ch}' at position {i}"
+            open_ch, open_pos = stack.pop()
+            if pairs[ch] != open_ch:
+                return (
+                    f"Mismatched '{open_ch}' at position {open_pos} "
+                    f"closed by '{ch}' at position {i}"
+                )
+
+    if stack:
+        open_ch, open_pos = stack[-1]
+        return f"Unclosed '{open_ch}' opened at position {open_pos}"
+
+    return "All brackets are balanced"
+
+
+# Example usage
+expr = """list.countvalid(Samples.GetRange(Lsm.[CLI001CL01_R_113 Ferry Rd_MICRO].[CLI001CL01_R_113 Ferry Rd_MICRO].[E.coli_E_MF_Water_100ML_SMEWW 9222K [ELS]]], (0 - 6), 0))"""
+print(check_brackets(expr))
+
---
+++ README.md
@@ -0,0 +1,7 @@
+####################
+Author: Joe Kennedy
+Date:   28 Apr 2026
+####################
+
+CheckBracketsBalance: Checks that that the number of opening brackets, parenthesis and braces match the number of closing.
+
